0fa0a1ef32fde7dee5ecbbf3248ca6c6afadd9aa
STEFAN_HASLINGER <stefan.haslinger@mittenin.at>
2011-07-03 13:24:43 +0200

    Fix defekter merge
    # To cancel this commit, close this file without making a change. To commit,
    # write your message, then save and close the file.
    #
    # On branch master
    # Changes to be committed:
    #   (use "git reset HEAD <file>..." to unstage)
    #
    #	modified:   app/controllers/articles_controller.rb
    #

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -24,20 +24,20 @@ class ArticlesController < ApplicationController
       redirect_to(:action => 'index')
     end
   end
-  
+
   # GET /articles/1
   # GET /articles/1.xml
   def permalink
     @showarticle = Article.find(:first, :conditions => ['name = ? and auth_level <= ?', params[:id], @auth_show])
     begin
     end while @showarticle.content.sub!(/\[\[(\w+)\]\]/,'<a href="/articles/\\1/permalink">\\1</a>')
-    if @showarticle == nil     
+    if @showarticle == nil
       @showarticle = Article.new
       @showarticle.name = t('Sorry')
       @showarticle.content = t('access denied')
     end
     render "show_content"
-  end  
+  end
 
   # GET /articles/new
   # GET /articles/new.xml
@@ -117,27 +117,16 @@ class ArticlesController < ApplicationController
   # GET /articles/1.xml
   def show_content
     @showarticle = Article.find(:first, :conditions => ['id = ? and auth_level <= ?', params[:id], @auth_show])
-<<<<<<< HEAD
-    begin
-    end while @showarticle.content.sub!(/\[\[(\w+)\]\]/,'<a href="/articles/\\1/permalink">\\1</a>')
-    if @showarticle == nil     
-=======
     if @showarticle == nil
->>>>>>> 0821af8dde79d41ce47f7a1d3b07b6a870b98303
       @showarticle = Article.new
       @showarticle.name = t('Sorry')
       @showarticle.content = t('access denied')
+    else
+      begin
+      end while @showarticle.content.sub!(/\[\[(\w+)\]\]/,'<a href="/articles/\\1/permalink">\\1</a>')
     end
-    respond_to do |format|
-      format.html # show_article.html.erb
-      format.xml  { render :xml => @article }
-    end
-<<<<<<< HEAD
-  end  
- end
-=======
   end
-
 end
 
 >>>>>>> 0821af8dde79d41ce47f7a1d3b07b6a870b98303
+
