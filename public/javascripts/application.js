28d112aff31323f14a2f6afa7324c3e21ffb3914
Michael Stranka# <michael.stranka@mittenin.at>
2011-07-05 18:54:10 +0200

    dependent dropdown

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -9,3 +9,33 @@ setInterval(function(){
       afterFinish: function(){ imgs.slice(0,imgs.length-1).invoke('show');  } });
 }, 8000);
 
+//<script type="text/javascript">
+  function update_auswahl()
+    {
+    var co = document.menue.menue_the_controller;
+    var ac = document.menue.menue_the_action;
+
+ if (co.options
+    [co.selectedIndex].
+    value == "articles")
+    {
+    ac.options.length = 0;
+    ac.options[0] = new Option("show_content");
+    ac.options[1] = new Option("showme");
+    document.getElementById("the_id").show()
+    }
+    else if (co.options
+    [co.selectedIndex].
+    value == "")
+    {
+    document.getElementById("the_id").hide()
+    ac.options.length = 0;
+    }
+    else {
+    ac.options.length = 0;
+    ac.options[0] = new Option("index");
+    document.getElementById("the_id").hide()
+    }
+    }
+//</script>
+
