656800944378bfd6578332fdfbf3fc4342f58943
Michael Stranka# <michael.stranka@mittenin.at>
2011-07-18 10:39:47 +0200

    layout mittenin

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,9 +5,9 @@ class User < ActiveRecord::Base
 #  acts_as_authentic do |c|
 #    c.consecutive_failed_logins_limit = 5       #To help protect from brute force attacks you can set a limit on the allowed number of consecutive failed logins.
 #    c.consecutive_failed_logins_limit = 2       #Once the failed logins limit has been exceed, how long do you want to ban the user? This can be a temporary or permanent ban.
-##
-#
-#
+#    c.merge_validates_length_of_password_field_options({:minimum => 3})
+#    c.validates_length_of_login_field_options({:within => 3..100})
+#    c.login_field (:email)
 #  end                                          # the configuration block is optional
 
   has_one :billingaddress, :dependent => :destroy, :conditions => {:address_type => "Rechnungsadresse"}
