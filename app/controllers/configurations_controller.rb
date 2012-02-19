77b27a3fe6f2c6ed585f2cf2baf0321c45a2f537
Stefan Haslinger <stefan.haslinger@mittenin.at>
2012-02-19 11:39:00 +0100

    Alle views, die ich einfach umstellen kann ....

--- app/controllers/configurations_controller.rb
+++ app/controllers/configurations_controller.rb
@@ -25,38 +25,10 @@ class ConfigurationsController < ApplicationController
     end
   end
 
-  # GET /configurations/new
-  # GET /configurations/new.xml
-  def new
-    @configuration = Configuration.new
-    @articles = Article.find(:all)
-    
-    respond_to do |format|
-      format.html # new.html.erb
-      format.xml  { render :xml => @configuration }
-    end
-  end
-
   # GET /configurations/1/edit
   def edit
     @configuration = Configuration.find(params[:id])
     @articles = Article.find(:all)
-  end
-
-  # POST /configurations
-  # POST /configurations.xml
-  def create
-    @configuration = Configuration.new(params[:configuration])
-
-    respond_to do |format|
-      if @configuration.save
-        format.html { redirect_to(@configuration, :notice => Configuration.human_name + ' ' + t('was successfully created')) }
-        format.xml  { render :xml => @configuration, :status => :created, :location => @configuration }
-      else
-        format.html { render :action => "new" }
-        format.xml  { render :xml => @configuration.errors, :status => :unprocessable_entity }
-      end
-    end
   end
 
   # PUT /configurations/1
@@ -72,18 +44,6 @@ class ConfigurationsController < ApplicationController
         format.html { render :action => "edit" }
         format.xml  { render :xml => @configuration.errors, :status => :unprocessable_entity }
       end
-    end
-  end
-
-  # DELETE /configurations/1
-  # DELETE /configurations/1.xml
-  def destroy
-    @configuration = Configuration.find(params[:id])
-    @configuration.destroy
-
-    respond_to do |format|
-      format.html { redirect_to(configurations_url) }
-      format.xml  { head :ok }
     end
   end
   
