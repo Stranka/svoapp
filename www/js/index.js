7cdb7edc53b0ec9d053b313befbb3aea724c76d3
Stranka$ <stranka@mittenin.at$>
2015-07-27 19:43:44 +0200

    added plugin network

--- www/js/index.js
+++ www/js/index.js
@@ -23,12 +23,13 @@
     // Cordova is loaded and it is now safe to make calls Cordova methods
     //
     function onDeviceReady() {
-      window.location="http://www.sv-olbendorf.at/mobiles/starting_point";
+//      window.location="http://www.sv-olbendorf.at/mobiles/starting_point";
 //        blinker();
-//        checkConnection();
+        checkConnection();
     }
 
     function checkConnection() {
+      alert('1111');
       var networkState = navigator.network.connection.type;
 
       var states = {};
@@ -39,7 +40,7 @@
       states[Connection.CELL_3G]    = 'Cell 3G connection';
       states[Connection.CELL_4G]    = 'Cell 4G connection';
       states[Connection.NONE]       = 'No network connection';
-
+      alert(networkState);
     	if (networkState == Connection.NONE) {
     	  alert('Keine Internetverbindung');
     	  //window.location="local/index.html";
