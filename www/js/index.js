c8270c4f71de52753390c9613df159a23b94129a
Stranka$ <stranka@mittenin.at$>
2015-10-23 09:22:03 +0200

    crosswalk

--- www/js/index.js
+++ www/js/index.js
@@ -50,8 +50,8 @@
 
         //window.location="local/index.html";
       } else {
-        window.open('http://www.sv-olbendorf.at/mobiles', '_self', 'location=no');
-//        window.location.href="http://www.sv-olbendorf.at/mobiles";
+//        window.open('http://www.sv-olbendorf.at/mobiles', '_self', 'location=no');
+        window.location.href="http://www.sv-olbendorf.at/mobiles";
       }
     }
 
